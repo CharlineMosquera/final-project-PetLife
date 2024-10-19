f75b72be4405cd00d28d963df7f8877b56bf3646
Tatiana Tarazona <leydy.tarazona@gmail.com>
2024-10-18 23:35:56 -0500

    Se agrega ojo para ocultar o ver la contraseña

--- js/scriptLogin.js
+++ js/scriptLogin.js
@@ -60,3 +60,19 @@ document
     // Lo envia a la pagina de inicio
     window.location.href = "../html/index.html";
   });
+
+// Obtener elementos del DOM
+const passwordInput = document.getElementById('password');
+const togglePassword = document.getElementById('toggle-password');
+
+// Agregar evento de clic para alternar la visibilidad de la contraseña
+togglePassword.addEventListener('click', function () {
+    // Verifica si el campo de contraseña está en modo "password"
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text'; // Cambiar a texto para mostrar la contraseña
+        togglePassword.textContent = '🙈'; // Cambiar el ícono a un "ojo cerrado"
+    } else {
+        passwordInput.type = 'password'; // Cambiar a password para ocultar la contraseña
+        togglePassword.textContent = '👁️'; // Cambiar el ícono a un "ojo abierto"
+    }
+});
--- js/scriptRegistro.js
+++ js/scriptRegistro.js
@@ -152,3 +152,34 @@ document
     // Lo envia a la pagina de login
     window.location.href = "../html/login.html";
   });
+
+
+// Obtener elementos del DOM
+const passwordInput = document.getElementById('password');
+const passwordInputConfirm = document.getElementById('password-confirm');
+const togglePassword = document.getElementById('toggle-password');
+const togglePasswordConfirm = document.getElementById('toggle-password-confirm');
+
+// Agregar evento de clic para alternar la visibilidad de la contraseña
+togglePassword.addEventListener('click', function () {
+    // Verifica si el campo de contraseña está en modo "password"
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text'; // Cambiar a texto para mostrar la contraseña
+        togglePassword.textContent = '🙈'; // Cambiar el ícono a un "ojo cerrado"
+    } else {
+        passwordInput.type = 'password'; // Cambiar a password para ocultar la contraseña
+        togglePassword.textContent = '👁️'; // Cambiar el ícono a un "ojo abierto"
+    }
+});
+
+// Agregar evento de clic para alternar la visibilidad de la contraseña de confirmacion
+togglePasswordConfirm.addEventListener('click', function () {
+    // Verifica si el campo de contraseña está en modo "password"
+    if (passwordInputConfirm.type === 'password') {
+        passwordInputConfirm.type = 'text'; // Cambiar a texto para mostrar la contraseña
+        togglePasswordConfirm.textContent = '🙈'; // Cambiar el ícono a un "ojo cerrado"
+    } else {
+        passwordInputConfirm.type = 'password'; // Cambiar a password para ocultar la contraseña
+        togglePasswordConfirm.textContent = '👁️'; // Cambiar el ícono a un "ojo abierto"
+    }
+});
